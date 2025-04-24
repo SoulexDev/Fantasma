3e583268d42b1aeea007e39b07a6fc63b0ad1c21
SoulexDev <fnafiplayar@gmail.com>
2025-04-23 17:08:28 -0700

    Update .gitignore, CodeChunks.db, CodeChunks.db-shm, and 18 more files

--- obj/Debug/netcoreapp3.1/Fantasma.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/Fantasma.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fantasma")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ca28b52467d02fb8e4e1354cd71f8dacc60ded")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ee00989e43fb570ad2a5162cb0cbf89ca251964")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fantasma")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fantasma")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
